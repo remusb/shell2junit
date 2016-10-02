c5e8bd1b87d8e1eff3d6f673f9d1eb39b9184fda
remusb <remus.bunduc@gmail.com>
2016-10-02 16:54:17 +0300

    Update sh2ju.sh

--- sh2ju.sh
+++ sh2ju.sh
@@ -107,7 +107,7 @@ juLog() {
   echo "+++ exit code: $evErr"        | tee -a $outf
 
   # set the appropriate error, based in the exit code and the regex
-  [ $evErr != 0 ] && err=1 || err=0
+  [[ $evErr != 0 ]] && err=1 || err=0
   out=`cat $outf | sed -e 's/^\([^+]\)/| \1/g'`
   if [ $err = 0 -a -n "$ereg" ]; then
       H=`echo "$out" | egrep $icase "$ereg"`
